6ff3b65fe64198090bab64715364c617b311491b
Josh <jlockrow@gmail.com>
2018-12-19 14:19:38 -0600

    Chore: Add index.html, made sure that CheeseController compiled correctly

--- src/main/java/com/example/cheesemvc/controllers/CheeseController.java
+++ src/main/java/com/example/cheesemvc/controllers/CheeseController.java
@@ -1,4 +1,14 @@
 package com.example.cheesemvc.controllers;
 
-public class CheeseController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class CheeseController<controller> {
+    @RequestMapping(value = "")
+    @ResponseBody
+    public String index() {
+        return "My Cheese";
+    }
 }
