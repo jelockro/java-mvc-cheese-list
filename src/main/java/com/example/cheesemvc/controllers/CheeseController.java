21a27cd39a52c34095dc2d8b591e5085c108a582
Josh <jlockrow@gmail.com>
2019-01-02 16:29:29 -0600

    feat: user input creates cheese instances

--- src/main/java/com/example/cheesemvc/controllers/CheeseController.java
+++ src/main/java/com/example/cheesemvc/controllers/CheeseController.java
@@ -1,5 +1,6 @@
 package com.example.cheesemvc.controllers;
 
+import com.example.cheesemvc.models.Cheese;
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 
@@ -8,20 +9,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
 
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
+    static HashMap<String, Cheese> cheeseObjects = new HashMap<>();
     static HashMap<String, String> cheeses = new HashMap<>();
+
 
     // request path: /cheese
     @RequestMapping(value = "")
-//    @ResponseBody
     public String index(Model model) {
 
+        /* Display content of cheeseObjects using Iterator*/
+        for (Cheese c:cheeseObjects.values()){
+            cheeses.put(c.getName(), c.getDescription());
+        }
         model.addAttribute("cheeses", cheeses);
         model.addAttribute("title", "My Cheeses");
         return "cheese/index";
@@ -35,7 +40,9 @@ public class CheeseController {
     }
     @RequestMapping(value = "add", method = RequestMethod.POST)
     public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDescription){
-        cheeses.put(cheeseName, cheeseDescription);
+        String cheeseObjectName = cheeseName;
+        Cheese cheeseobjectName = new Cheese(cheeseName, cheeseDescription);
+        cheeseObjects.put(cheeseName, cheeseobjectName);
 
         // Redirect to /cheese
         return "redirect:";
@@ -51,7 +58,7 @@ public class CheeseController {
     @RequestMapping(value = "remove", method = RequestMethod.POST)
     public String processRemoveCheeseFrom(@RequestParam ArrayList<String> cheeseCheckbox){
         for(String item : cheeseCheckbox) {
-            cheeses.remove(item);
+            cheeseObjects.remove(item);
         }
 
         // Redirect to /cheese
--- src/main/java/com/example/cheesemvc/models/Cheese.java
+++ src/main/java/com/example/cheesemvc/models/Cheese.java
@@ -4,15 +4,15 @@ public class Cheese {
     private String name;
     private String description;
 
-    protected Cheese(String name, String description){
+    public Cheese(String name, String description){
         this.name = name;
         this.description = description;
     }
 
-    protected String getName(){
+    public String getName(){
         return(this.name);
     }
-    protected String getDescription(){
+    public String getDescription(){
         return(this.description);
     }
 }
