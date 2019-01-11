5c028cd12d1dc6ce2102a3bb612af6fa9848030c
Josh <jlockrow@gmail.com>
2019-01-10 18:47:01 -0600

    adds default constructor to pass empty values

--- src/main/java/com/example/cheesemvc/models/Cheese.java
+++ src/main/java/com/example/cheesemvc/models/Cheese.java
@@ -4,6 +4,11 @@ public class Cheese {
     private String name;
     private String description;
 
+    public Cheese() {
+        this.name = "";
+        this.description = "";
+    }
+
     public Cheese(String name, String description){
         this.name = name;
         this.description = description;
