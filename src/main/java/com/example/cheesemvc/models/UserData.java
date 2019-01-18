65fda74181e0fab7584e40fbc60571961cc6ef9e
Josh <jlockrow@gmail.com>
2019-01-17 21:29:51 -0600

    Feat: Adds user helper functions

--- src/main/java/com/example/cheesemvc/models/UserData.java
+++ src/main/java/com/example/cheesemvc/models/UserData.java
@@ -1,4 +1,36 @@
 package com.example.cheesemvc.models;
 
+import java.util.ArrayList;
+
 public class UserData {
+    static ArrayList<User> users= new ArrayList<>();
+    // get all
+    public static ArrayList<User> getAll() {
+        return users;
+    }
+    // add
+    public static void add(User newUser){
+        users.add(newUser);
+    }
+    // remove
+    public static void remove(int id) {
+        User userToRemove = getByID(id);
+        users.remove(userToRemove);
+    }
+    // getbyID
+    public static User getByID(int id) {
+        User theUser = null;
+
+        for (User candidateUser: users) {
+            if (candidateUser.getUserID() == id) {
+                theUser = candidateUser;
+            }
+        }
+        return theUser;
+    }
+    public static void edit(User editUser) {
+        remove(editUser.getUserID());
+        add(editUser);
+
+    }
 }
